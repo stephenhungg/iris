4ba170931c9babc8c8e47ce4389990a1876e88d4
Stephen Hung <stephenhung@berkeley.edu>
2026-04-19 08:52:10 -0700

    fix: polyfill crypto.randomUUID for non-secure contexts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,11 @@
+// polyfill crypto.randomUUID for non-secure contexts (HTTP)
+if (typeof crypto !== 'undefined' && !crypto.randomUUID) {
+  (crypto as Crypto & { randomUUID: () => string }).randomUUID = () =>
+    '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c: string) =>
+      (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16)
+    );
+}
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
